0880500230058d4a5535a765f7a266f1540a0fb9
Anrich Potgieter <anrichp@gmail.com>
2019-03-13 10:16:47 +0200

    Added app.run to itemCatalog.py

--- itemCatalog.py
+++ itemCatalog.py
@@ -8,3 +8,6 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Item=Item, Category=Category)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
