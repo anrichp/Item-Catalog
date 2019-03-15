624aa6eb4031e01e13a3ad0038bda1d12fea7786
Anrich Potgieter <anrichp@gmail.com>
2019-03-16 00:54:45 +0200

    Updated populateDB to insert permissions on files

--- populateDB.py
+++ populateDB.py
@@ -10,6 +10,14 @@ category6 = Category(name="Fujifilm")
 category7 = Category(name="GoPro")
 category8 = Category(name="Leica")
 
+createdById1 = "anrichp@gmail.com"
+createdById2 = "staceyclaudine@gmail.com"
+createdById3 = "hatfieldchristianchurcheast@gmail.com"
+
+createdBy1 = "Anrich Potgieter"
+createdBy2 = "Stacey Potgieter"
+createdBy3 = "Hatfield Christian Church"
+
 db.session.add(category1)
 db.session.add(category2)
 db.session.add(category3)
@@ -20,88 +28,77 @@ db.session.add(category7)
 db.session.add(category8)
 
 item1 = Item(name="Canon Rebel SL2",
-             description='''Best all-round: Canon's smallest DSLR blends
-                            versatility and value.''',
-             category=category1)
+             description='''Best all-round: Canon's smallest DSLR blends versatility and value''',
+             category=category1, createdById=createdById1,
+             createdBy=createdBy1)
 item2 = Item(name="Canon EOS R",
-             description='''Runner-up: Canon's first full-frame mirrorless
-                            camera means business''',
-             category=category1)
+             description='''Runner-up: Canon's first full-frame mirrorless camera means business''',
+             category=category1, createdById=createdById1,
+             createdBy=createdBy1)
 item3 = Item(name="Canon EOS 5D Mark IV",
-             description='''From the moment light passes through the lens,the
-                            EOS 5D Mark IV captures every nuance, every colour,
-                            every detail.''',
-             category=category1)
+             description='''From the moment light passes through the lens,the EOS 5D Mark IV captures every nuance, every colour, every detail.''',
+             category=category1, createdById=createdById1,
+             createdBy=createdBy1)
 
 item4 = Item(name="Nikon D3X",
-             description='''The Nikon D3X is a 24.4-megapixel professional
-                            -grade full-frame digital single-lens
-                            reflex camera''',
-             category=category2)
+             description='''The Nikon D3X is a 24.4-megapixel professional-grade full-frame digital single-lens reflex camera''',
+             category=category2, createdById=createdById1,
+             createdBy=createdBy1)
 
 item5 = Item(name="Nikon D2Xs",
-             description='''find out how the D2Xs stacks up against the
-                            competition in our real-world review with
-                            in-depth image quality comparisons.''',
-             category=category2)
+             description='''find out how the D2Xs stacks up against the competition in our real-world review with in-depth image quality comparisons.''',
+             category=category2, createdById=createdById2,
+             createdBy=createdBy2)
 
 item6 = Item(name="Sony A580",
-             description='''The 16.2MP Sony A580 is based around one of the
-                          company's Exmor APS HD CMOS sensors''',
-             category=category4)
+             description='''The 16.2MP Sony A580 is based around one of the company's Exmor APS HD CMOS sensors''',
+             category=category4, createdById=createdById2,
+             createdBy=createdBy2)
 
 item7 = Item(name="Pentax 645Z",
-             description='''The Pentax 645Z is a professional medium format
-                            digital SLR camera announced by Ricoh''',
-             category=category3)
+             description='''The Pentax 645Z is a professional medium format digital SLR camera announced by Ricoh''',
+             category=category3, createdById=createdById2,
+             createdBy=createdBy2)
 item8 = Item(name="Pentax K-1",
-             description='''Sporting Pentax's first full-frame digital image
-                            sensor, the K-1 DSLR Camera is a landmark release
-                            for the respected company''',
-             category=category3)
+             description='''Sporting Pentax's first full-frame digital image sensor, the K-1 DSLR Camera is a landmark release for the respected company''',
+             category=category3, createdById=createdById2,
+             createdBy=createdBy2)
 
 item9 = Item(name="Olympus OM-D E‑M5",
-             description='''The Olympus OM-D E-M1 Mark II is a high performance
-                            mirrorless digital camera sporting a 20.4MP
-                            live MOS Micro Four Thirds sensor''',
-             category=category5)
+             description='''The Olympus OM-D E-M1 Mark II is a high performance mirrorless digital camera sporting a 20.4MP live MOS Micro Four Thirds sensor''',
+             category=category5, createdById=createdById2,
+             createdBy=createdBy2)
 item10 = Item(name="Olympus OM-D E‑M1",
-              description='''Powering the OM-D E-M5 Mark II is Olympus's own 16
-                              megapixel Live MOS sensor and TruePic
-                              VII image processor''',
-              category=category5)
+              description='''Powering the OM-D E-M5 Mark II is Olympus's own 16 megapixel Live MOS sensor and TruePi VII image processor''',
+              category=category5, createdById=createdById3,
+              createdBy=createdBy3)
 
 item11 = Item(name="fujifilm xt20",
-              description='''The Fujifilm XT 20 mirrorless camera features a
-                              2.36M-dot organic EL electronic viewfinder''',
-              category=category6)
+              description='''The Fujifilm XT 20 mirrorless camera features a 2.36M-dot organic EL electronic viewfinder''',
+              category=category6, createdById=createdById3,
+              createdBy=createdBy3)
 item12 = Item(name="fujifilm xt2",
-              description='''The Fujifilm X-T2 is a compact, lightweight, and
-                              high-performance mirrorless camera that includes
-                              all the features that a modern
-                              photographer needs.''',
-              category=category6)
+              description='''The Fujifilm X-T2 is a compact, lightweight, and high-performance mirrorless camera that includes all the features that a modern photographer needs.''',
+              category=category6, createdById=createdById3,
+              createdBy=createdBy3)
 item13 = Item(name="fujifilm x100f",
-              description='''The Fujifilm X100F is a powerful premium compact
-                              camera with a beautifully designed
-                              and compact body.''',
-              category=category6)
+              description='''The Fujifilm X100F is a powerful premium compact camera with a beautifully designed and compact body.''',
+              category=category6, createdById=createdById3,
+              createdBy=createdBy3)
 
 item14 = Item(name="GoPro HERO5 Black",
-              description='''The GoPro HERO5 Black is the ultimate action camera.
-                              Everything of the previous camera's been revised,
-                              improving video/image quality''',
-              category=category7)
+              description='''The GoPro HERO5 Black is the ultimate action camera. Everything of the previous camera's been revised, improving video/image quality''',
+              category=category7, createdById=createdById3,
+              createdBy=createdBy3)
 item15 = Item(name="GoPro HERO4 Session",
-              description='''HERO4 Session packs the power of GoPro into our
-                              smallest, lightest, most convenient camera
-                              yet—featuring a rugged and waterproof design''',
-              category=category7)
+              description='''HERO4 Session packs the power of GoPro into our smallest, lightest, most convenient camera yet—featuring a rugged and waterproof design''',
+              category=category7, createdById=createdById3,
+              createdBy=createdBy3)
 
 item16 = Item(name="Leica SL",
-              description='''The Leica SL-System is the embodiment of the
-                             digital era in professional photography''',
-              category=category8)
+              description='''The Leica SL-System is the embodiment of the digital era in professional photography''',
+              category=category8, createdById=createdById3,
+              createdBy=createdBy3)
 
 
 db.session.add(item1)
